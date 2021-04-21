d57b8cf81790b0700d6c900f3574cbc721bec943
angelicakeys <75345767+angelicakeys@users.noreply.github.com>
2021-04-20 23:11:50 -0400

    depot de l'exercice 3

---
+++ theme4w4/template-parts/content-cours-article.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Template part l'affichage des bloc de cours dans front-page.php
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package theme4w4
+ */
+global $tPropriété;
+?>
+
+
+<article>
+<div class="divArticle"> <!-- class hover animation -->
+	<p><?php echo $tPropriété['sigle'] . " - " . $tPropriété['typeCours'] . " - " . $tPropriété['nbHeure'] ; ?></p>
+	<a href="<?php echo get_permalink() ?>"><?php echo $tPropriété['titrePartiel']; ?></a>
+	<p>Session : <?php echo $tPropriété['session']; ?></p>
+	</div> <!-- class hover animation -->
+</article>
